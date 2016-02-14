812c8e86bb1ba255bf3b68b3e7094e0da5dce285
Laure Kamalandua <laure.kamalandua@synergics.be>
2016-02-14 20:39:22 +0100

    first commit

---
+++ Get-Inspired.ps1
@@ -0,0 +1,56 @@
+# -------------------------------------------------  Author        :   Laure Kamalandua  ------------------------------------------------- #
+# -------------------------------------------------  Contact       :   laure.kamalandua@gmail.com  --------------------------------------- #
+# -------------------------------------------------  Version       :   1                 ------------------------------------------------- #
+# -------------------------------------------------  Description   :   Fetches a random inspirational quote ------------------------------ #
+
+function Get-Inspired() {
+    $APIurl = 'http://www.forismatic.com/api/1.0/'
+    $client = New-Object System.Net.WebClient
+    $client.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
+    $client.Encoding = [System.Text.Encoding]::UTF8
+    [xml]$quote = $client.UploadString($apiUrl, 'method=getQuote&lang=en&format=xml')
+    $quoteFull = $quote.forismatic.quote.quotetext +"- " + $quote.forismatic.quote.quoteauthor
+    $quoteAuthor = $quote.forismatic.quote.quoteauthor
+    $quoteChars = $quoteFull.Length
+    $quoteArray = $quoteFull.Split()
+    $quoteWords = ($quoteFull | Measure-Object -Word).Words
+    $splitIndex = ($quoteWords/2)
+    $splitWord = $quoteArray[$splitIndex]
+    $splitWordFloor = [math]::floor($splitIndex)
+    $correctIndex = $quoteFull.LastIndexOf($splitWord)
+    if ($quoteChars -gt 66 -AND $quoteChars -lt 180){
+        'Quote Chars: ' + $quoteChars
+        'Quote Words: ' + $quoteWords
+        'Split Index: ' + $splitIndex
+        'Split Word: ' + $splitWord
+        'Split World Floor: ' + $splitWordFloor
+        'Correct Index: ' + $correctIndex
+        'Quote Author: ' + $quoteAuthor
+        'Quote: ' + $quoteFull
+        $finalQuote = $quoteFull.Insert($correctIndex, "       `r`n" + '       ')
+        "`r`n" + '       ' + '*' * $correctIndex +"`r`n"
+        Write-Host '      ' $finalQuote -ForegroundColor Yellow
+        "`r`n" + '       ' + '*' * $correctIndex + "`r`n"
+        break
+    } elseif ($quoteChars -gt 0 -AND $quoteChars -lt 65) {
+        'Quote Chars: ' + $quoteChars
+        'Quote Words: ' + $quoteWords
+        'Quote Author: ' + $quoteAuthor
+        "`r`n" + '       ' + '*' * $quoteChars +"`r`n"
+        Write-Host '      ' $quoteFull -ForegroundColor Yellow
+        "`r`n" + '       ' + '*' * $quoteChars + "`r`n"
+        break
+    } else {
+        'Quote Chars: ' + $quoteChars
+        'Quote Words: ' + $quoteWords
+        'Quote Author: ' + $quoteAuthor
+        "`r`n" + '       ' + '*' * $quoteChars +"`r`n"
+        Write-Host '      ' $quoteFull -ForegroundColor Yellow
+        "`r`n" + '       ' + '*' * $quoteChars + "`r`n"
+        break
+    }
+}
+
+Get-Inspired
+
+
