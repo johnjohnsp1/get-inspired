9abfdc0d2046ed08dfdba564084457473295de9c
Laure Kamalandua <laure.kamalandua@synergics.be>
2016-02-15 01:08:40 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Get-Inspired
+The best inspirational quotes, right in your Powershell console.
+
+[![Join the chat at https://gitter.im/LaureKamalandua/laurekamalandua](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/Zhouzi/TheaterJS?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+![Alt text](/images/icon.png)
+
+
+## Contents
+- [**Installation**](##installation)
+- [**Usage**](##usage)
+- [**Notes**](##notes)
+
+## Installation
+
+* git clone 
+
+## Usage
+### Run the script from your shell to get a random quote.
+```powershell
+PS C:\System32\> .\Get-Inspiration.ps1
+
+       ***************************************************
+       
+       My mama always said: life's like a box of chocolate
+       — you never know what you gonna get.- Forrest Gump
+      
+       ***************************************************
+       
+```
+### Run the Start-Init function to add the script to your Powershell profile. A new quote will be visible every time you open the shell.
+```powershell
+PS C:\Users\User\Downloads\get-inspired\> Import-Module -Name .\Get-Inspiration.ps1 
+PS C:\Users\User\Downloads\get-inspired\> Start-Init
+The Powershell profile exists and the module will be added.
+The module has been succesfully added to your profile.
+       
+```
+
+## Notes
+If you don't have a Windows Powershell profile set up yet, I kindly point to [this](http://www.howtogeek.com/126469/how-to-create-a-powershell-profile/) link.
+
+## Change Log
+
+* [ ] port to bash for UNIX users
+* [ ] enhance string formatting
+* [ ] enable quotes with a greater character length than 180 
+
+
+## Credits
+
+Built with ❤ by [Laure Kamalandua](http://laurekamalandua.com)<br>
+Twitter ([@laurekamalandua](http://twitter.com/laurekamalandua))<br>
+Contact [laure.kamalandua@gmail.com](mailto:laure.kamalandua@gmail.com)
--- script/Get-Inspired.ps1
+++ script/Get-Inspired.ps1
@@ -1,5 +1,6 @@
 # -------------------------------------------------  Author        :   Laure Kamalandua  ------------------------------------------------- #
 # -------------------------------------------------  Contact       :   laure.kamalandua@gmail.com  --------------------------------------- #
+# -------------------------------------------------  Website       :   http://www.laurekamalandua.com  ----------------------------------- #
 # -------------------------------------------------  Version       :   1 ----------------------------------------------------------------- #
 # -------------------------------------------------  Description   :   Fetches a random inspirational quote ------------------------------ #
 
@@ -10,7 +11,6 @@ function Get-Inspired() {
     $client.Encoding = [System.Text.Encoding]::UTF8
     [xml]$quote = $client.UploadString($apiUrl, 'method=getQuote&lang=en&format=xml')
     $quoteFull = $quote.forismatic.quote.quotetext +"- " + $quote.forismatic.quote.quoteauthor
-    $quoteAuthor = $quote.forismatic.quote.quoteauthor
     $quoteChars = $quoteFull.Length
     $quoteArray = $quoteFull.Split()
     $quoteWords = ($quoteFull | Measure-Object -Word).Words
@@ -23,20 +23,29 @@ function Get-Inspired() {
         "`r`n" + '       ' + '*' * $correctIndex +"`r`n"
         Write-Host '      ' $finalQuote -ForegroundColor Yellow
         "`r`n" + '       ' + '*' * $correctIndex + "`r`n"
-        break
     } elseif ($quoteChars -gt 0 -AND $quoteChars -lt 65) {
         "`r`n" + '       ' + '*' * $quoteChars +"`r`n"
         Write-Host '      ' $quoteFull -ForegroundColor Yellow
         "`r`n" + '       ' + '*' * $quoteChars + "`r`n"
-        break
     } else {
         "`r`n" + '       ' + '*' * $quoteChars +"`r`n"
         Write-Host '      ' $quoteFull -ForegroundColor Yellow
         "`r`n" + '       ' + '*' * $quoteChars + "`r`n"
-        break
     }
 }
 
 Get-Inspired
 
+function Start-Init {
+    $script = $PSScriptRoot + '\get-inspired.ps1'
+    $documents = (Get-ChildItem env:).userprofile + '\Documents\' 
+    $documentPowershell = $documents + '\WindowsPowerShell\Microsoft.PowerShell_profile.ps1'
+    if (((Get-ChildItem $profile -ErrorAction SilentlyContinue | select -Property *).Exists) -eq $True) {
+        Write-Host 'The Powershell profile exists and the module will be added.' -ForegroundColor Green 
+        $scriptcontent = Get-Content $script -Head 34 -Force | Select-Object -Skip 4 | Add-Content $profile -Force
+        Write-Host 'The module has been succesfully added to your profile.' -ForegroundColor Green    
+    } else {
+        Write-Host 'Your Powershell profile does not exist.'`n'Create it before running this script.' -ForegroundColor Red
+    }
+}
 
